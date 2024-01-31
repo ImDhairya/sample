c1b041fc76a11903e9ff7d019e8399b4e8b838ea
Dhairya <pandyadhairya789@gmail.com>
2024-01-31 18:42:32 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,16 @@
 import express from "express";
 import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
+app.use(express.json);
 
 app.get("/", (req, res) => {
   res.send("Hello world ");
